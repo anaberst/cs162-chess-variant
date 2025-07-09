e13f7f3e227ca1a9779a60a3665528647cbce2ef
anaberst <anavberst@gmail.com>
2025-07-09 15:11:02 -0700

    Initialize ChessPiece class

--- chess_var.py
+++ chess_var.py
@@ -2,3 +2,22 @@
 # GitHub username: anaberst
 # Date: 03/16/2025 (uploaded to GitHub: 07/09/2025)
 # Description: This program simulates a game of the chess variant "King of the Hill".
+
+class ChessPiece:
+    """
+    Represents a chess piece.
+    Parent for all chess piece classes.
+    """
+
+    def __init__(self, color):
+        """
+        Initializes a ChessPiece object with a color attribute
+        """
+        self._color = color.lower()  # 'black' or 'white'
+
+    def get_color(self):
+        """
+        Returns the chess piece's color
+        """
+        return self._color
+    
