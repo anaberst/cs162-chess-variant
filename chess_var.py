c65074a07f13dfa958be9a2ebca89ab8cc7b4578
anaberst <anavberst@gmail.com>
2025-07-09 15:22:00 -0700

    Style: remove extra space and period

--- chess_var.py
+++ chess_var.py
@@ -1,14 +1,13 @@
 # Author: Anastasiya Berst
 # GitHub username: anaberst
 # Date: 03/16/2025 (uploaded to GitHub: 07/09/2025)
-# Description: This program simulates a game of the chess variant "King of the Hill".
+# Description: This program simulates a game of the chess variant "King of the Hill"
 
 class ChessPiece:
     """
     Represents a chess piece.
     Parent for all chess piece classes.
     """
-
     def __init__(self, color):
         """
         Initializes a ChessPiece object with a color attribute
