9808bf86ed52c0ce6d359b00d3cfd9524ad9dec6
anaberst <anavberst@gmail.com>
2025-07-09 15:03:59 -0700

    Add mandatory CS162 header

---
+++ chess_var.py
@@ -0,0 +1,4 @@
+# Author: Anastasiya Berst
+# GitHub username: anaberst
+# Date: 03/16/2025 (uploaded to GitHub: 07/09/2025)
+# Description: This program simulates a game of the chess variant "King of the Hill".
