98fa2e498b6c2cfdb8c9aad3692ade93ea9c4444
anaberst <anavberst@gmail.com>
2025-07-10 17:06:09 -0700

    Update GitHub upload date

--- chess_var.py
+++ chess_var.py
@@ -1,6 +1,6 @@
 # Author: Anastasiya Berst
 # GitHub username: anaberst
-# Date: 03/16/2025 (uploaded to GitHub: 07/09/2025)
+# Date: 03/16/2025 (uploaded to GitHub: 07/10/2025)
 # Description: This program simulates a game of the chess variant "King of the Hill"
 
 class ChessPiece:
--- test_chess_var.py
+++ test_chess_var.py
@@ -1,4 +1,4 @@
 # Author: Anastasiya Berst
 # GitHub username: anaberst
-# Date: 03/16/2025 (uploaded to GitHub: 07/09/2025)
+# Date: 03/16/2025 (uploaded to GitHub: 07/10/2025)
 # Description: This program contains unit tests for chess_var.py
